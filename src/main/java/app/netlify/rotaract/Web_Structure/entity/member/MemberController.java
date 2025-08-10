db2b699003380bd3d48de1ec24a21e04e80772d0
Luiz Antônio Gonçalves <antonio.luiz@ufpr.br>
2025-08-10 14:18:06 -0300

    add docker file and CORS

--- src/main/java/app/netlify/rotaract/Web_Structure/entity/member/MemberController.java
+++ src/main/java/app/netlify/rotaract/Web_Structure/entity/member/MemberController.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/member")
+@CrossOrigin(origins = "http://localhost:4200")
 public class MemberController {
     private final MemberService memberService;
 
